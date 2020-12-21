0f017518c420cfc7b487d5352752b07219fedc42
0918nobita <nobita.0918@gmail.com>
2020-12-21 23:35:43 +0900

    :sparkles: redux-saga のミドルウェア・rootSagaを追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,30 @@
 import { useCallback } from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider, useDispatch, useSelector } from 'react-redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import Keyboard from './keyboard';
 import { initialState, reducer } from './store';
 
+const composeEnhancers =
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (window as any)?.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
   reducer,
   initialState,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (window as any)?.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(sagaMiddleware))
 );
+
+// eslint-disable-next-line require-yield
+function* rootSaga() {
+  console.log('Hello, redux-saga!');
+}
+
+sagaMiddleware.run(rootSaga);
 
 const Counter: React.VFC = () => {
   const count = useSelector((state) => state.count);
