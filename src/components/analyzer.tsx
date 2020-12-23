be6986d948e7300cc13a100c892a11d217093905
0918nobita <nobita.0918@gmail.com>
2020-12-23 11:53:09 +0900

    :construction: canvas アニメーションのサンプルを追加

--- src/components/analyzer.tsx
+++ src/components/analyzer.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react';
 
 export const Analyzer: React.VFC = () => {
   const ref = useRef<HTMLCanvasElement>(null);
+  const requestRef = useRef<number>();
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -11,6 +12,21 @@ export const Analyzer: React.VFC = () => {
     ctx.fillStyle = 'white';
     ctx.font = '48px serif';
     ctx.fillText('Synth', 10, 50);
+
+    const animate = () => {
+      const x = Math.floor(Math.random() * 300);
+      const y = Math.floor(Math.random() * 200);
+      ctx.fillRect(x, y, 1, 1);
+
+      requestRef.current = requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    return () => {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      cancelAnimationFrame(requestRef.current!);
+    };
   }, []);
 
   return <canvas ref={ref} width={300} height={200} />;
