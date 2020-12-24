def31ffaecbec6daa8e3b24ed48a99e88e64a337
0918nobita <nobita.0918@gmail.com>
2020-12-24 21:30:09 +0900

    :sparkles: MIDI キーボードから操作できるようにした

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,9 +1,11 @@
 import { fork } from 'redux-saga/effects';
 
 import { keyboard } from './keyboard';
+import { midi } from './midi';
 
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
 export function* rootSaga() {
   yield fork(keyboard);
+  yield fork(midi);
 }
