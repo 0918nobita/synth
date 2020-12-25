41a37a7b7826f380cb1b030a294e842d3bda0cf3
0918nobita <nobita.0918@gmail.com>
2020-12-25 10:08:49 +0900

    :lipstick: SVGKeyboard の各鍵盤にホバー時のスタイルシートを追加した

--- src/components/svgKeyboard/index.tsx
+++ src/components/svgKeyboard/index.tsx
@@ -1,19 +1,28 @@
+import styles from './SVGKeyboard.css';
+
 export const SVGKeyboard: React.VFC = () => (
   <svg width={210} height={110} viewBox={[0, 0, 210, 110].join(', ')}>
     {[0, 30, 60, 90, 120, 150, 180].map((x) => (
       <rect
         key={x}
+        className={styles.whiteKey}
         x={x}
         y={0}
         width={30}
         height={110}
         stroke="black"
         strokeWidth={1}
-        fill="white"
       />
     ))}
     {[20, 52, 108, 142, 174].map((x) => (
-      <rect key={x} x={x} y={0} width={18} height={67} fill="rgb(58, 58, 58)" />
+      <rect
+        key={x}
+        className={styles.blackKey}
+        x={x}
+        y={0}
+        width={18}
+        height={67}
+      />
     ))}
   </svg>
 );
