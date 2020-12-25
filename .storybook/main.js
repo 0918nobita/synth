8fe43e41407d51a84b0ad5d1f9fda0a3b26fa92c
0918nobita <nobita.0918@gmail.com>
2020-12-25 10:10:08 +0900

    :wrench: Storybook 側の webpack 設定を修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,5 +6,19 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    const rules = [
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader?modules'],
+      },
+    ];
+
+    return { ...config, module: { ...config.module, rules }};
+  },
 };
